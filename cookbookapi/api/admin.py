d2c16f7425e826e791d6a0feb5455902a34c895e
alon gouldman <alongouldman@gmail.com>
2019-04-11 16:06:38 +0300

    recipe model added

--- cookbookapi/api/admin.py
+++ cookbookapi/api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from cookbookapi.api.models import Recipe
 
-# Register your models here.
+admin.site.register(Recipe)
--- cookbookapi/api/models.py
+++ cookbookapi/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
-# Create your models here.
+
+def get_image_path(instance, filename):
+    return os.path.join('photos', str(instance.id), filename)
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=32)
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+
+
