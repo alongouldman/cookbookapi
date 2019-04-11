6a6a040f3d15a5651cf7d6891dca2bab3fec52a0
alon gouldman <alongouldman@gmail.com>
2019-04-11 18:40:31 +0300

    cors

--- cookbookapi/api/serializers.py
+++ cookbookapi/api/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from .models import Recipe
 from .models import Ingredient
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
--- cookbookapi/urls.py
+++ cookbookapi/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -6,35 +5,16 @@ from cookbookapi.api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
-
-
-router = routers.DefaultRouter()
 router.register(r'Recipes', views.RecipeViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
-
-
-router = routers.DefaultRouter()
 router.register(r'Ingredients', views.IngredientViewSet)
 
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    ]
+]
+
+
