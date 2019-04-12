368496a7a359147a1cdf600102a51dd7fa8907bd
naorvilensky <naor@furiouscorp.com>
2019-04-12 07:54:22 +0300

    fixed urls

--- cookbookapi/api/urls.py
+++ cookbookapi/api/urls.py
@@ -4,7 +4,7 @@ from cookbookapi.api import views
 urlpatterns = [
     # path('recipes/', views.recipe_list),
     # path('', include(router.urls)),
-    path(r'recipes/', views.RecipeList.as_view()),
+    path(r'recipes', views.RecipeList.as_view()),
     # path(r'recipes/', views.RecipeList.as_view()),
     re_path(r'ingredients', views.IngredientList.as_view()),
     # path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework'))
